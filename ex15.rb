9af850ec25d7f4e505fcada2b0bb2dee172a725a
KhalilC <klcrumpler@gmail.com>
2014-05-23 19:51:25 -0400

    added ex15.rb

---
+++ ex15.rb
@@ -0,0 +1,17 @@
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if { |word| word.start_with?("s") }
+
+puts arr
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if { |word| word.start_with?("s", "w")}
+
+puts arr
+
+
+
+
+
+
