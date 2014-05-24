e600dc07d43e14033950ce91dbe716f2e1b31e06
KhalilC <klcrumpler@gmail.com>
2014-05-23 22:33:59 -0400

    added ex16.rb

---
+++ ex16.rb
@@ -0,0 +1,13 @@
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+     
+
+new_array = a.map {|word| word.split(' ')}
+
+p new_array.flatten
+
+     
+
+
+
+
